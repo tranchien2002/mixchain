e9fc5be3f6c83dcbe0756fc4d41eda27faee1912
Sotatek-DucPham <duc.pham@sotatek.com>
2018-11-28 23:46:42 +0700

    demo get data

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -1,11 +1,30 @@
 'use strict';
 
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const AdminConnection = require('composer-admin').AdminConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+
 class UserController {
   async getUser({ response, auth }) {
     try {
-      const { email, username } = await auth.getUser();
-      return { email, username };
+      const { id, email, username } = await auth.getUser();
+
+      //connect to network with cardId
+      let businessNetworkConnection = new BusinessNetworkConnection();
+      await businessNetworkConnection.connect('' + id);
+
+      //get member from the network
+      let namespace = 'com.mixchain';
+      const memberRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Member');
+      const member = await memberRegistry.get('' + id);
+
+      //disconnect
+      await businessNetworkConnection.disconnect('' + id);
+
+      return member;
     } catch (error) {
+
+      console.log(error);
       response.send('Missing or invalid jwt token');
     }
   }
